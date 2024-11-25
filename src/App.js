df564791f2044f40f893d88357a3552470535d46
Adham901 <118096712+Adham901@users.noreply.github.com>
2024-11-25 16:35:08 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,71 +1,120 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import photo from "./photo_2024-09-29_00-52-59.jpg";
+import Form from "react-bootstrap/Form";
+import Accordion from "react-bootstrap/Accordion";
+import { question } from "./components/data";
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const person = [
-    { id: "0", name: "حسن", date: "5 مساء", img: "p1.jpg" },
-    { id: "1", name: "محمد", date: "4 مساء", img: "p1.jpg" },
-    { id: "2", name: "على", date: "3 مساء", img: "p1.jpg" },
-    { id: "3", name: "محمود", date: "2 مساء", img: "p1.jpg" },
-    { id: "4", name: "حسن", date: "1 مساء", img: "p1.jpg" },
-    { id: "5", name: "حسن", date: "1 مساء", img: "p1.jpg" },
-    { id: "6", name: "حسن", date: "1 مساء", img: "p1.jpg" },
-  ];
+const [data , setData]=useState(question)
+const [qu , setQ]=useState("")
+const [an, setA]=useState("")
 
-  const [view, setView] = useState(person);
+const additem =()=>{
+  if(qu === "" || an === "" ){
+    notify("من فضلك اكمل البيانات" , "Error")
+    return;
+  
+  }
+    question.push({id : Math.random() , q : qu , a : an})
+    setA('')
+    setQ('')
+    onAdd()
+    
+}
 
-  const deletView = () => {
-    setView([]);
-  };
+const onAdd = ()=>{
+  setData([...question])
+  notify("ـم الاضافه بنجاح" , "Success")
+}
 
-  const showView = () => {
-    setView(person);
-  };
+const deletALLData =()=>{
+  question.splice(0 , question.length)
+  setData([])
+  notify("ـم المسح بنجاح" , "Error")
+}
+
+const deletoneitem = (question) => {
+  setData([...question])
+  notify("ـم المسح بنجاح" , "Error")
+}
+
+const deletItem = (ID)=>{
+
+  if (data.length >= 1){
+    const index = question.findIndex((item)=> item.id === ID)
+    question.splice(index,1)
+    deletoneitem(question)
+  }
+
+
+}
+
+
+const notify = (message ,type) => {
+  if (type === "Error"){
+    toast.error(message)
+   
+  }
+  else if (type === "Success"){
+
+    toast.success(message)
+  }
+};
 
   return (
     <>
-      <div className="d-flex justify-content-center">
-        <Container className="m-5   ">
-          <Row className="text-center  mb-3 title">
-            <Col >
-              <h5>لديك اليوم {person.length} مواعيد</h5>
-            </Col>
-          </Row>
-
-          <Row className="content">
-            <Col>
-              <div className="rectangler p-3 ">
-                {person.length ? (
-                  view.map((item) => {
-                    return (
-                      <div key={item.id} className="d-flex">
-                        <img src={photo} className="img-avatar" alt=" " />
-                        <div className="px-2 ">
-                          <p className="d-inline fs-5">{item.name}</p>
-                          <p className="fs-6">{item.date}</p>
-                        </div>
-                      </div>
-                    );
-                  })
-                ) : (
-                  <h1>ليس هناك مواعيد الان </h1>
-                )}
-              </div>
-            </Col>
-          </Row>
-
+      <div className="m-5">
+        <Container>
           <Row>
-            <Col className="d-flex justify-content-between pt-2">
-              <button onClick={showView} className="btn btn-success">
-                عرض البيانات
-              </button>
-              <button onClick={deletView} className="btn btn-danger">
-                مسح الكل
-              </button>
+            <Col sm="4">
+              <p className="fs-3 text-center">اسئله واجوبه شائعه</p>
+            </Col>
+            <Col sm="8">
+              <Row>
+                <Col sm="5">
+                  <Form.Control value={qu} onChange={(e)=>{setQ( e.target.value)}} type="text" placeholder="ادخل االسوال" />
+                </Col>
+                <Col sm="5">
+                  {" "}
+                  <Form.Control value={an} onChange={(e)=>{setA( e.target.value)}}  type="text" placeholder="ادخل الاجابه" />
+                </Col>
+                <Col sm="2">
+                  <button onClick={additem} className="btn btn-success  w-100 ">اضافه</button>
+                </Col>
+              </Row>
+              <Row className="mt-2">
+                <Accordion>
+                  {data.length >= 1 ? (
+                    question.map((item , index ) => {
+                      return (
+                        <Accordion.Item key={index} eventKey={item.id}>
+                          <Accordion.Header>
+                            <div className="m-auto">{item.q}</div>
+                          </Accordion.Header>
+                          <Accordion.Body>
+                            <div className="d-inline px-3">{item.a}</div>
+                            <button onClick={() => deletItem(item.id)} className="btn btn-outline-danger">
+                              مسح السوال
+                            </button>
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      );
+                    })
+                  ) : (
+                    <h1 className="text-center m-5">لا يوجد اساله</h1>
+                  )}
+                </Accordion>
+              </Row>
+
+              {question.length >= 1 ? (
+                <button onClick={deletALLData} className="btn btn-success w-100 mt-2">مسح الكل</button>
+              ) : null}
             </Col>
           </Row>
+          <ToastContainer />
         </Container>
       </div>
     </>
